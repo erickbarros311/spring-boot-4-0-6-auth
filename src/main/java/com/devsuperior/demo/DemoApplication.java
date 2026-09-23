c35e17317a1d35289ee5fbde96acf73716ee6c7c
Erick <erickbarros311@gmail.com>
2026-09-23 00:02:57 -0300

    BCrypt Password

--- src/main/java/com/devsuperior/demo/DemoApplication.java
+++ src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,13 +1,55 @@
 package com.devsuperior.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication {
-
+public class DemoApplication{
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	
+
 }
+
+
+
+/*
+
+EXEMPLO PARA TESTAR O HASH DE SENHA PARA O BANCO, SENHA: 123456
+ HASH DO ALGORITMO BCRYPT $2a$10$7iw7W9M9lJZRr5u6ZfkL..waksjr27DKFdYKczo/LkCU6veHavbAS
+
+ 
+ -------------------
+ 
+ @SpringBootApplication
+public class DemoApplication implements CommandLineRunner{
+
+	// injetando uma instancia do passwordencoder
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("\nENCODE = " + passwordEncoder.encode("123456"));
+		
+		boolean result = passwordEncoder.matches("123456", "$2a$10$7iw7W9M9lJZRr5u6ZfkL..waksjr27DKFdYKczo/LkCU6veHavbAS");
+		
+		System.out.println("\nRESULTADO = " + result);
+	}
+
+}
+
+
+
+*/
